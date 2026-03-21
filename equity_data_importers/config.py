784d51bb8d97e5a7dbd6ab7def230d3105bc0eb2
filip19992 <filipwlodarczyk99@gmail.com>
2026-03-21 14:32:27 +0100

    Initial commit

---
+++ equity_data_importers/config.py
@@ -0,0 +1,11 @@
+import datetime as dt
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Config:
+    COMPANY_NAME: str = "Tesla"
+    TICKER: str = "TSLA"
+    GDELT_QUERY: str = "(Tesla OR TSLA)"
+    GEO: str = "US"
+    START_DATE: dt.date = dt.date(2023, 1, 1)
+    END_DATE: dt.date = dt.date(2026, 1, 1)
