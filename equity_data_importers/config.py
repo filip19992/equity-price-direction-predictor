ab8f7575fab1a05a762808504602e9f333580dac
filip19992 <filipwlodarczyk99@gmail.com>
2026-03-23 23:49:24 +0100

    update daterange in config for importers

--- equity_data_importers/config.py
+++ equity_data_importers/config.py
@@ -8,4 +8,4 @@ class Config:
     GDELT_QUERY: str = "(Tesla OR TSLA)"
     GEO: str = "US"
     START_DATE: dt.date = dt.date(2023, 1, 1)
-    END_DATE: dt.date = dt.date(2026, 1, 1)
+    END_DATE: dt.date = dt.date(2025, 12, 31)
